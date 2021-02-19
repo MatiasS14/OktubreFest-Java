e749a5ce1c0b6c189ac4d5ec2baa0ab8ac669934
Matias Solanes <matias.solanes14@gmail.com>
2021-02-19 19:11:49 -0300

    Clase Jarra implementada

--- src/oktubreFest/Jarra.java
+++ src/oktubreFest/Jarra.java
@@ -1,5 +1,20 @@
 package oktubreFest;
 
 public class Jarra {
+	private Marca marcaCerveza;
+	private Integer capacidadJarra;//capacidad en litros
+	
+	public Jarra(Integer capacidad, Marca marcaCerveza) {
+		this.marcaCerveza = marcaCerveza;
+		this.capacidadJarra = capacidad;
+	}
+	
+	public Marca marcaCerveza() {
+		return this.marcaCerveza;
+	}
+	
+	public Integer capacidadJarra() {
+		return this.capacidadJarra;
+	}
 
 }
