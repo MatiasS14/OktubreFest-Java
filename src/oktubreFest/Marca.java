eecff864aebe42bcf3670130844e61523844f2fa
Matias Solanes <matias.solanes14@gmail.com>
2021-02-19 19:12:05 -0300

    Clase Marca implementada

--- src/oktubreFest/Marca.java
+++ src/oktubreFest/Marca.java
@@ -1,5 +1,22 @@
 package oktubreFest;
 
 public abstract class Marca {
-
+	protected Integer gramosLupuloPorLitro;//gramos de lupulo por litro de cerveza
+	protected String paisFabricacion;
+	
+	public Marca(Integer lupuloPorLitro, String paisFabricacion) {
+		this.gramosLupuloPorLitro = lupuloPorLitro;
+		this.paisFabricacion = paisFabricacion;
+	}
+	
+	public String paisFabricacion() {
+		return this.paisFabricacion;
+	}
+	
+	public Integer cantidadLupulo() {
+		return this.gramosLupuloPorLitro;		
+	}
+	
+	public abstract Float graduacionDeAlcohol() ;
+	
 }
